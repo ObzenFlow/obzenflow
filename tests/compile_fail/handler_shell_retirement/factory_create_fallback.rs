64b4e225c611e6ed069c655f96e2c99d860da0fd
Kevin Webber <kevin@obzenflow.dev>
2026-07-26 21:25:54 -0400

    added spdx headers
    Signed-off-by: Kevin Webber <kevin@obzenflow.dev>

--- tests/compile_fail/handler_shell_retirement/factory_create_fallback.rs
+++ tests/compile_fail/handler_shell_retirement/factory_create_fallback.rs
@@ -1,3 +1,7 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+// SPDX-FileCopyrightText: 2025-2026 ObzenFlow Contributors
+// https://obzenflow.dev
+
 use obzenflow_adapters::middleware::control::ControlMiddlewareAggregator;
 use obzenflow_adapters::middleware::MiddlewareFactory;
 use obzenflow_runtime::pipeline::config::StageConfig;
