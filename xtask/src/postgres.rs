f9f1878785afea21055957191c90415f1cfa7e3b
Kevin Webber <kevin@obzenflow.dev>
2026-08-23 11:46:05 -0400

    fixed spdx header
    Signed-off-by: Kevin Webber <kevin@obzenflow.dev>

--- xtask/src/postgres.rs
+++ xtask/src/postgres.rs
@@ -1,3 +1,7 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+// SPDX-FileCopyrightText: 2025-2026 ObzenFlow Contributors
+// https://obzenflow.dev
+
 use super::{error, Result};
 #[cfg(unix)]
 use std::sync::atomic::{AtomicI32, Ordering};
